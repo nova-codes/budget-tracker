b0ea45092c76ec31dc21bec257e19624924e7083
Hannah Elliott <hgelliott90@gmail.com>
2021-01-30 12:46:25 -0500

    finish service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,3 +41,36 @@ self.addEventListener("activate", function (evt) {
 
 self.ClientRectList.claim();
 });
+
+// fetch data from API
+self.addEventListener("fetch", function(evt) {
+  const {url} = evt.request;
+  if (url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone and store in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // If the network request fails, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+  } else {
+    // respond from static cache, request is not for /api/*
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  }
+});
