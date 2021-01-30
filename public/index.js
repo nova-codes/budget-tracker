39ea4409d66c9998f7dc165ccd7178f7d295b9ee
Hannah Elliott <hgelliott90@gmail.com>
2021-01-30 14:01:37 -0500

    styling

--- public/index.js
+++ public/index.js
@@ -71,7 +71,7 @@ function populateChart() {
         datasets: [{
             label: "Total Over Time",
             fill: true,
-            backgroundColor: "#6666ff",
+            backgroundColor: "rgba(226,105,148,0.6)",
             data
         }]
     }
